8c9b088ddc9edf5578361937977472fc9307040d
kantoo <keren@la-mark.com>
2013-02-04 12:12:45 +0100

    Update cookbooks/lb_haproxy/metadata.rb

--- cookbooks/lb_haproxy/metadata.rb
+++ cookbooks/lb_haproxy/metadata.rb
@@ -2,7 +2,7 @@ maintainer       "RightScale, Inc."
 maintainer_email "support@rightscale.com"
 license          "All rights reserved"
 description      "Override RightScale HAProxy configuration"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description "Description"
 version          "0.0.1"
 
 depends "rs_utils"
